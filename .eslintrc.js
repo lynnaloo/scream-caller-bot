0d7f845074b95fb00a8bdb26b2afffb2537d10f7
Linda Nichols <lynnaloo@gmail.com>
2020-10-20 10:30:48 -0400

    eslint and configs clean-up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,27 @@
-/* eslint-disable */
 module.exports = {
-    "extends": "standard",
-    "rules": {
-        "semi": [2, "always"],
-        "indent": [2, 4],
-        "no-return-await": 0,
-        "space-before-function-paren": [2, {
-            "named": "never",
-            "anonymous": "never",
-            "asyncArrow": "always"
-        }],
-        "template-curly-spacing": [2, "always"]
-    }
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: 'plugin:shopify/esnext',
+  rules: {
+    'no-console': 0,
+    'comma-dangle': 0,
+    quotes: [2, 'single', {
+      avoidEscape: true,
+      allowTemplateLiterals: true
+    }],
+    'no-unused-vars': ['error', {
+      vars: 'local',
+      args: 'none'
+    }],
+    'no-process-env': 0,
+    'template-curly-spacing': [2, 'always'],
+    'space-before-function-paren': [2, {
+      named: 'never',
+      anonymous: 'never',
+      asyncArrow: 'always'
+    }],
+    'no-return-await': 0
+  }
 };
--- index.js
+++ index.js
@@ -1,59 +1,59 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-const appInsights = require('applicationinsights');
+
 const path = require('path');
 
 const dotenv = require('dotenv');
-// Import required bot configuration.
-const ENV_FILE = path.join(__dirname, '.env');
-dotenv.config({ path: ENV_FILE });
-
+const appInsights = require('applicationinsights');
 const restify = require('restify');
-const { BotFrameworkAdapter } = require('botbuilder');
+const {BotFrameworkAdapter} = require('botbuilder');
+
+const ENV_FILE = path.join(__dirname, '.env');
+dotenv.config({path: ENV_FILE});
 
 // This bot's main dialog
-const { ScreamBot } = require('./bot');
+const {ScreamBot} = require('./bot');
 
 // Create HTTP server
 const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-    appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
-        .setAutoDependencyCorrelation(true)
-        .setAutoCollectRequests(true)
-        .setAutoCollectPerformance(true, true)
-        .setAutoCollectExceptions(true)
-        .setAutoCollectDependencies(true)
-        .setAutoCollectConsole(true)
-        .setUseDiskRetryCaching(true)
-        .setSendLiveMetrics(false)
-        .setDistributedTracingMode(appInsights.DistributedTracingModes.AI)
-        .start();
+  appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
+    .setAutoDependencyCorrelation(true)
+    .setAutoCollectRequests(true)
+    .setAutoCollectPerformance(true, true)
+    .setAutoCollectExceptions(true)
+    .setAutoCollectDependencies(true)
+    .setAutoCollectConsole(true)
+    .setUseDiskRetryCaching(true)
+    .setSendLiveMetrics(false)
+    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI)
+    .start();
 
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
-    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+  console.log(`\n${ server.name } listening to ${ server.url }`);
+  console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+  console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
 });
 
 const adapter = new BotFrameworkAdapter({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    channelService: process.env.ChannelService,
-    openIdMetadata: process.env.BotOpenIdMetadata
+  appId: process.env.MicrosoftAppId,
+  appPassword: process.env.MicrosoftAppPassword,
+  channelService: process.env.ChannelService,
+  openIdMetadata: process.env.BotOpenIdMetadata
 });
 
 // Map knowledge base endpoint values from .env file into the required format for `QnAMaker`
 const configuration = {
-    knowledgeBaseId: process.env.QnAKnowledgebaseId,
-    endpointKey: process.env.QnAAuthKey,
-    host: process.env.QnAEndpointHostName
+  knowledgeBaseId: process.env.QnAKnowledgebaseId,
+  endpointKey: process.env.QnAAuthKey,
+  host: process.env.QnAEndpointHostName
 };
 
 // Catch-all for errors.
 const onTurnErrorHandler = async (context, error) => {
-    console.error(`\n [onTurnError] unhandled error: ${ error }`);
+  console.error(`\n [onTurnError] unhandled error: ${ error }`);
 
     // Send a trace activity, which will be displayed in Bot Framework Emulator
-    await context.sendTraceActivity(
+  await context.sendTraceActivity(
         'OnTurnError Trace',
         `${ error }`,
         'https://www.botframework.com/schemas/error',
@@ -61,7 +61,7 @@ const onTurnErrorHandler = async (context, error) => {
     );
 
     // Send a message to the user
-    await context.sendActivity('The bot encountered an error or bug.');
+  await context.sendActivity('The bot encountered an error or bug.');
 };
 
 // Set the onTurnError for the singleton BotFrameworkAdapter
@@ -72,21 +72,21 @@ const screamBot = new ScreamBot(configuration, {});
 
 // Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
+  adapter.processActivity(req, res, async (context) => {
         // Route to main dialog.
-        await screamBot.run(context);
-    });
+    await screamBot.run(context);
+  });
 });
 
 // Listen for Upgrade requests for Streaming.
 server.on('upgrade', (req, socket, head) => {
-    const streamingAdapter = new BotFrameworkAdapter({
-        appId: process.env.MicrosoftAppId,
-        appPassword: process.env.MicrosoftAppPassword
-    });
-    streamingAdapter.onTurnError = onTurnErrorHandler;
-    streamingAdapter.useWebSocket(req, socket, head, async (context) => {
+  const streamingAdapter = new BotFrameworkAdapter({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+  });
+  streamingAdapter.onTurnError = onTurnErrorHandler;
+  streamingAdapter.useWebSocket(req, socket, head, async (context) => {
         // After connecting via WebSocket, run this logic for every request sent over the WebSocket connection
-        await screamBot.run(context);
-    });
+    await screamBot.run(context);
+  });
 });
