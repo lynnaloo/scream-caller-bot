3f3720282b90115122db755eacac34746b947d29
Linda Nichols <lynnaloo@gmail.com>
2020-10-20 10:38:46 -0400

    more eslint clean-up

--- PostDeployScripts/mergeSettings.js
+++ PostDeployScripts/mergeSettings.js
@@ -1,6 +1,7 @@
-/* eslint-disable */
+/* eslint-disable no-shadow */
 const fs = require('fs');
 const path = require('path');
+
 const appSettingPath = path.resolve('./appsettings.json');
 const envPath = path.resolve('./.env');
 
@@ -8,53 +9,53 @@ const settings = readSettingsFile(appSettingPath, envPath);
 const env = process.env;
 
 const envNames = Object.keys(env)
-    .filter(function(x){ return x.indexOf("APPSETTING_") === 0;})
-    .filter(function(x){ return x.indexOf("APPSETTING_WEBSITE_") < 0; });
-const appSettings = envNames.reduce(function(cur, key) { return {...cur, [key.substring(11)]: env[key]};}, {});
+  .filter((x) => { return x.indexOf('APPSETTING_') === 0; })
+  .filter((x) => { return x.indexOf('APPSETTING_WEBSITE_') < 0; });
+const appSettings = envNames.reduce((cur, key) => { return {...cur, [key.substring(11)]: env[key]}; }, {});
 
 const newSettings = {...settings, ...appSettings};
 
 updateSettingsFile(appSettingPath, envPath, newSettings);
 
 function readDotEnv(file) {
-    const text = fs.readFileSync(file, 'utf-8');
-    const lines = text.split(/[\r\n]/);
-    const settings = lines.reduce(function(cur, line) {
-        const parts = line.split('=');
-        if (parts.length >= 2) {
-            const name = parts.splice(0, 1);
-            cur[name] = parts.join('=');
-        }
-        return cur;
-    }, {});
+  const text = fs.readFileSync(file, 'utf-8');
+  const lines = text.split(/[\r\n]/);
+  const settings = lines.reduce((cur, line) => {
+    const parts = line.split('=');
+    if (parts.length >= 2) {
+      const name = parts.splice(0, 1);
+      cur[name] = parts.join('=');
+    }
+    return cur;
+  }, {});
 
-    return settings;
+  return settings;
 }
 
 function readSettingsFile(appSettingPath, envPath) {
-    if (fs.existsSync(appSettingPath)) {
-        return JSON.parse(fs.readFileSync(appSettingPath, 'utf-8'));
-    }
+  if (fs.existsSync(appSettingPath)) {
+    return JSON.parse(fs.readFileSync(appSettingPath, 'utf-8'));
+  }
 
-    if (fs.existsSync(envPath)) {
-        return readDotEnv(envPath);
-    }
+  if (fs.existsSync(envPath)) {
+    return readDotEnv(envPath);
+  }
 
-    return {};
+  return {};
 }
 
 function isNodeJSProject() {
-    return fs.existsSync(path.resolve('./iisnode.yml'));
+  return fs.existsSync(path.resolve('./iisnode.yml'));
 }
 
 function updateSettingsFile(appSettingPath, envPath, settings) {
-    if (isNodeJSProject()) {
-        const keys = Object.keys(settings);
-        const lines = keys.reduce(function(cur, key) {
-            return [...cur, key + '=' + settings[key]];
-        }, []);
-        fs.writeFileSync(envPath, lines.join('\n'), {encoding: 'utf-8'});
-    } else {
-        fs.writeFileSync(appSettingPath, JSON.stringify(settings, null, 2), {encoding: 'utf-8'});
-    }
+  if (isNodeJSProject()) {
+    const keys = Object.keys(settings);
+    const lines = keys.reduce((cur, key) => {
+      return [...cur, `${ key }=${ settings[key] }`];
+    }, []);
+    fs.writeFileSync(envPath, lines.join('\n'), {encoding: 'utf-8'});
+  } else {
+    fs.writeFileSync(appSettingPath, JSON.stringify(settings, null, 2), {encoding: 'utf-8'});
+  }
 }
